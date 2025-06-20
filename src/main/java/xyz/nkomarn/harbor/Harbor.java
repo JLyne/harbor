2659abf01804568d40f52bf7c05ae940c227a1d9
James Lyne <jim+github@not-null.co.uk>
2025-06-20 16:29:42 +0100

    Brigadier commands

--- src/main/java/xyz/nkomarn/harbor/Harbor.java
+++ src/main/java/xyz/nkomarn/harbor/Harbor.java
@@ -1,16 +1,24 @@
 package xyz.nkomarn.harbor;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import xyz.nkomarn.harbor.api.ExclusionProvider;
-import xyz.nkomarn.harbor.command.ForceSkipCommand;
-import xyz.nkomarn.harbor.command.HarborCommand;
 import xyz.nkomarn.harbor.task.Checker;
 import xyz.nkomarn.harbor.util.Config;
 import xyz.nkomarn.harbor.util.Messages;
+
+import static io.papermc.paper.command.brigadier.Commands.literal;
 
 @SuppressWarnings("UnstableApiUsage")
 public class Harbor extends JavaPlugin {
@@ -28,8 +36,9 @@ public class Harbor extends JavaPlugin {
 
         pluginManager.registerEvents(messages, this);
 
-        getCommand("harbor").setExecutor(new HarborCommand(this));
-        getCommand("forceskip").setExecutor(new ForceSkipCommand(this));
+        LifecycleEventManager<@NotNull Plugin> manager = getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.COMMANDS,
+                                     event -> registerCommands(event.registrar()));
     }
 
     @Override
@@ -39,9 +48,43 @@ public class Harbor extends JavaPlugin {
         }
     }
 
-    @NotNull
-    public String getVersion() {
-        return getPluginMeta().getVersion();
+    private void registerCommands(Commands commands) {
+        LiteralArgumentBuilder<CommandSourceStack> reloadCommand = literal("reload")
+                .requires(source -> source.getSender().hasPermission("harbor.admin"))
+                .executes(ctx -> {
+                    config.reload();
+                    ctx.getSource().getSender().sendRichMessage(config.getPrefix() + "Reloaded configuration.");
+                    return Command.SINGLE_SUCCESS;
+                });
+
+        LiteralArgumentBuilder<CommandSourceStack> forceSkipCommand = literal("forceskip")
+                .requires(source -> source.getSender().hasPermission("harbor.forceskip"))
+                .executes(ctx -> {
+                    if (!(ctx.getSource().getSender() instanceof Player player)) {
+                        ctx.getSource().getSender()
+                                .sendRichMessage(config.getPrefix() + "This command can only be used by a player.");
+                        return Command.SINGLE_SUCCESS;
+                    }
+
+                    World world = player.getWorld();
+
+                    if (getChecker().isSkipping(world)) {
+                        player.sendRichMessage(config.getPrefix() + "This world's time is already being accelerated.");
+                    } else {
+                        player.sendRichMessage(config.getPrefix() + "Forcing night skip in your world.");
+                        getChecker().forceSkip(world);
+                    }
+
+                    return Command.SINGLE_SUCCESS;
+                });
+
+        commands.register(literal("harbor")
+                                  .requires(source ->
+                                                    source.getSender().hasPermission("harbor.admin")
+                                                            || source.getSender().hasPermission("harbor.forceskip"))
+                                  .then(reloadCommand)
+                                  .then(forceSkipCommand)
+                                  .build(), "Main command for Harbor");
     }
 
     @NotNull
